3cf9077b60cd5f779ac505b8aa98be308c645919
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2023-06-02 00:17:49 +0800

    Updated regex pattern.

--- src/Application/Features/XmlAutoEscaper/Constants/TextEditorConstant.cs
+++ src/Application/Features/XmlAutoEscaper/Constants/TextEditorConstant.cs
@@ -1,5 +1,5 @@
 namespace Obaki.Toolkit.Application.Features.XmlAutoEscaper.Services.TextEditor;
 public static class TextEditorConstants
 {
-    public const string XmlRegexPattern =@"(?<=(<(\w+)>))(?<value>.*?)(?=(</(\w+)>))|(?<=(\s*=\s*['""]))(?<value>.*?)(?=(?:['""]\s*/>|['""]\s*\w+\s*=\s*))";
+    public const string XmlRegexPattern =@"(?<=<(\w+)>)(?<value>.*?)(?=</\1>)|(?<=\s*=\s*['""])(?<value>.*?)(?=(?:['""]\s*/>|['""]\s*\w+\s*=\s*))";
 }
